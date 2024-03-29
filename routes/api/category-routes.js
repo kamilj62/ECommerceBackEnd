56e16026c84a8f419d8bc75b30c7e80674c9d2e8
Joseph Kamil <kamilj@umich.edu>
2024-03-29 09:06:00 -0700

    fixed categories delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -67,6 +67,7 @@ router.delete("/:id", async (req, res) => {
     if (!result) {
       return res.status(404).json({ msg: "Category not found" });
     }
+    res.status(200).json(result);
   } catch (err) {
     res.status(500).json(err);
   }
